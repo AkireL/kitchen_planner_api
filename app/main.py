7c344417768f4f9e40bd2ddaf698dd7707963444
Erika Leonor Basurto Munguia <iamdleonor@gmail.com>
2025-02-19 12:10:15 -0600

    setup rate limit

--- app/main.py
+++ app/main.py
@@ -3,6 +3,7 @@ from fastapi import FastAPI
 from fastapi.security import OAuth2PasswordBearer
 
 from app.db import init_db
+from app.rate_limit import config_rate_limit
 from app.routers.authentication_router import auth_router
 from app.routers.recipe_router import recipe_router
 
@@ -15,6 +16,7 @@ def create_application() -> FastAPI:
     return application
 
 app = create_application()
+config_rate_limit(app)
 
 @app.on_event("startup")
 async def startup_event():
--- app/rate_limit.py
+++ app/rate_limit.py
@@ -1,10 +1,12 @@
 from fastapi import FastAPI
-from slowapi import Limiter
+from slowapi import Limiter, _rate_limit_exceeded_handler
+from slowapi.errors import RateLimitExceeded
 from slowapi.middleware import SlowAPIMiddleware
 from slowapi.util import get_remote_address
 
 
 def config_rate_limit(app: FastAPI) -> None:
-    limiter = Limiter(key_func=get_remote_address, default_limits=["5/minute"])
+    limiter = Limiter(key_func=get_remote_address, default_limits=["1000/minute"])
     app.state.limiter = limiter
+    app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
     app.add_middleware(SlowAPIMiddleware)
