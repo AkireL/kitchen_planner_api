ae7b6ff253d81273b2db14632b61752d397b8bd8
Erika Leonor Basurto Munguia <iamdleonor@gmail.com>
2025-02-18 22:34:30 -0600

    refactor code

--- app/services/recipe_service.py
+++ app/services/recipe_service.py
@@ -54,29 +54,22 @@ class RecipeService:
 
     @staticmethod
     async def filter_recipes(filters: RecipeFilterSchema, offset: int, per_page: int):
-        if filters is None:
-            queryset = Recipe.all()
-            return await queryset.limit(per_page).offset(offset)
-
-        query = Recipe.all()
-
-        if filters.title:
-            query = query.filter(title__icontains=filters.title)
-        if filters.duration:
-            query = query.filter(duration=filters.duration)
-        if filters.schedule_at:
-            query = query.filter(schedule_at=filters.schedule_at)
-        if filters.start_date and filters.end_date:
-            query = query.filter(schedule_at__range=(filters.start_date, filters.end_date))
-
-        query = query.offset(offset).limit(per_page)
+        query = RecipeService.scope_filter(filters)
+        query = query.limit(per_page).offset(offset)
         return await query
 
     @staticmethod
     async def get_count_recipes_to_filter(filters: RecipeFilterSchema):
+        query = RecipeService.scope_filter(filters)
+        query = query.count()
+
+        return await query
+
+
+    @staticmethod
+    def scope_filter(filters: RecipeFilterSchema):
         if filters is None:
-            query = Recipe.all().count()
-            return await query
+            return Recipe.all().count()
 
         query = Recipe.all()
 
@@ -91,6 +84,4 @@ class RecipeService:
                 schedule_at__gte=filters.start_date,
                 schedule_at__lte=filters.end_date
             )
-        query = query.count()
-
-        return await query
+        return query
