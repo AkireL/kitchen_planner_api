592d0b90806d1523b07d1fce65842901e53939d7
Miguel Angel Vázquez Vázquez <mvk_12@outlook.com>
2025-03-04 16:44:15 -0600

    use now + tz instead utcnow

--- app/services/auth_service.py
+++ app/services/auth_service.py
@@ -27,7 +27,7 @@ class AuthService:
     @staticmethod
     def create_access_token(data: dict, expires_delta: timedelta = None):
         to_encode = data.copy()
-        expire = datetime.utcnow() + (expires_delta or timedelta(minutes=15))
+        expire = datetime.now(datetime.timezone.utc) + (expires_delta or timedelta(minutes=15))
         to_encode.update({"exp": expire})
         return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
 
