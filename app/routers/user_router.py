19e85709543bf144b43241ea39d619ec568799ff
Erika Leonor Basurto Munguia <iamdleonor@gmail.com>
2025-03-06 21:40:43 -0600

    WIP

--- app/routers/user_router.py
+++ app/routers/user_router.py
@@ -1,7 +1,9 @@
-from typing import Annotated
 
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException, Query
 from fastapi.responses import JSONResponse
+from tortoise.expressions import Q
+
+from app.models import User
 from app.services.user_service import UserService
 
 user_router = APIRouter(prefix="/user")
@@ -13,3 +15,30 @@ async def list_user():
     return {
         "data": users
     }
+
+
+@user_router.get("/search")
+async def search_user(
+    username: str | None = Query(None, description="username", min_length=1),
+    email: str | None = Query(None, description="username", min_length=1),
+):
+    
+    if not username and not email:
+        raise HTTPException(status_code=400, detail="You must provide at least a value to search.")
+    
+    conditions = []
+
+    if username:
+        conditions.append(Q(username__icontains=username))
+    if email:
+        conditions.append(Q(email__icontains=email))
+
+    if conditions:
+        query = conditions[0] if len(conditions) == 1 else Q()
+        for condition in conditions:
+            query |= condition
+    else:
+        query = Q() 
+        
+    users = await User.filter(query).values("id", "username", "email", "fullname")
+    return JSONResponse(content={"data": users})
